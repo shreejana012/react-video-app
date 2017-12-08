d7e90159eeab209135f5ea0878ea89d80edb6f44
shreejana012 <shrijana460@gmail.com>
2017-12-08 16:10:08 +0545

    added lodash to delay some time in search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -31,9 +32,10 @@ class App extends Component {
   }
   
   render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300)
     return(
       <div>
-        <SearchBar onSelectTermChange= {videos => this.videoSearch(videos)}/>
+        <SearchBar onSelectTermChange= {videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList 
         onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
